a836667b7a02ab741e495e8f587af2122b1cf9e1
Vince001 <xcc001@qq.com>
2017-02-02 17:12:47 +0800

    [UART][Console] 1, The UART console was finished.

--- test/user/main.c
+++ test/user/main.c
@@ -16,9 +16,76 @@ void delay(uint32_t cnt)
 		    k++;
 }
 
+uint8_t UART_GetKey(void)
+{
+  while ( UARTCount == 0 );
+
+  LPC_UART->IER = IER_THRE | IER_RLS;			/* Disable RBR */
+  //UARTSend( (uint8_t *)UARTBuffer, UARTCount );
+  UARTCount = 0;
+  LPC_UART->IER = IER_THRE | IER_RLS | IER_RBR;	/* Re-enable RBR */
+
+  return UARTBuffer[0];
+}
+
+uint8_t UART_GetCmdAndData( uint8_t *buff, uint8_t *cnt )
+{
+    uint8_t i=0;
+    printf("Input:");
+
+  while ( UARTCount == 0 );
+
+  LPC_UART->IER = IER_THRE | IER_RLS;			/* Disable RBR */
+  UARTSend( (uint8_t *)UARTBuffer, UARTCount );
+  *cnt = UARTCount;
+  //printf("\n");
+  for(i=0;i<UARTCount;i++) 
+  {
+      buff[i] = UARTBuffer[i];
+      //printf("%x ",UARTBuffer[i]);
+  }
+  //printf("\n");
+  UARTCount = 0;
+  LPC_UART->IER = IER_THRE | IER_RLS | IER_RBR;	/* Re-enable RBR */
+
+  return UARTBuffer[0];
+}
+
+void Led_blink(void)
+{
+    uint32_t i = 0;
+    for(i=0;i<4;i++)
+	{
+	    GPIOSetValue( 2, i%4, 0 );
+        //GPIOSetValue( 2, 1, 0 );
+		//GPIOSetValue( 2, 2, 0 );
+        //GPIOSetValue( 2, 3, 0 );
+
+		delay( 1000 );
+
+		GPIOSetValue( 2, i%4, 1 );
+        //GPIOSetValue( 2, 1, 1 );
+		//GPIOSetValue( 2, 2, 1 );
+        //GPIOSetValue( 2, 3, 1 );
+
+		delay( 1000 );
+	}
+}
+
+void Led_off( void )
+{
+    GPIOSetDir( 2, 0, 0 );
+    GPIOSetDir( 2, 1, 0 );
+	GPIOSetDir( 2, 2, 0 );
+    GPIOSetDir( 2, 3, 0 );
+}
+
 int main(void)
 {
     uint32_t i = 0;
+    //uint8_t cnt = 0;
+    uint8_t cmd_index = 0;
+    uint8_t buff[50];
     SystemInit();
 
     /* NVIC is installed inside UARTInit file. */
@@ -29,25 +96,45 @@ int main(void)
 	GPIOSetDir( 2, 2, 1 );
     GPIOSetDir( 2, 3, 1 );
 
-	printf("Start begin sdk build \n");
+	printf("Start begin sdk build \r\n");
+    UARTSend( "haha test\r\n", 12 );
 
-	for(i=0;i<50000;i++)
-	{
-	    GPIOSetValue( 2, i%4, 0 );
-        //GPIOSetValue( 2, 1, 0 );
-		//GPIOSetValue( 2, 2, 0 );
-        //GPIOSetValue( 2, 3, 0 );
+    while(1)
+    {
+        buff[cmd_index] = UART_GetKey();
+        if(buff[cmd_index] != ';') // the end
+        {
+            cmd_index++;
+            if(cmd_index == BUFSIZE)
+                cmd_index = 0;
+                       
+            continue;
+        } 
 
-		delay( 5000 );
+        for(i=0;i<=cmd_index;i++)  // send back the cmd
+            printf("%c",buff[i]);
+        
+        printf("\nGet cmd\n");      
 
-		GPIOSetValue( 2, i%4, 1 );
-        //GPIOSetValue( 2, 1, 1 );
-		//GPIOSetValue( 2, 2, 1 );
-        //GPIOSetValue( 2, 3, 1 );
+        if(cmd_index < 2) // cmd too few
+            continue;
 
-		delay( 5000 );
-	}
-
-    return 0;
+        switch( buff[0] )
+        {
+            case 's':
+                GPIOSetValue( 2, buff[2]-'0', 0 );  
+            break;
+            case 'c':
+                GPIOSetValue( 2, buff[2]-'0', 1 );    
+            break;
+            default: break;                
+        }
+        for(i=0;i<UARTCount;i++) 
+        {
+            buff[i] = 0;
+            UARTBuffer[i] = 0;
+        }
+        cmd_index = 0;
+    }
 }
 
